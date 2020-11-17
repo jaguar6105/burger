7ea6df5ef17972e16e3c073d7fab49b8314c2d5e
jaguar6105 <gilljoseph603@gmail.com>
2020-11-17 17:28:16 -0500

    adding jawsDb

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,23 @@
 const mysql = require("mysql");
 
-const connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "",
-  database: "burgers_db"
-});
+let connection;
+
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  //otherwise, we're going to use our local connection!  put your local db set stuff here
+  //(and remember our best practice of using the dotenv package and a .env file ;)
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "",
+    database: "burgers_db"
+  });
+}
 
 // Make connection.
-connection.connect(function(err) {
+connection.connect(function (err) {
   if (err) {
     console.error("error connecting: " + err.stack);
     return;
