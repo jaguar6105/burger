63cb049c37c7711a6dcb1fe473fb9958bd09be50
jaguar6105 <gilljoseph603@gmail.com>
2020-11-17 16:17:34 -0500

    add server

---
+++ public/assets/js/burgers.js
@@ -0,0 +1,44 @@
+// Make sure we wait to attach our handlers until the DOM is fully loaded.
+$(function () {
+    $(".change-eat").on("click", function (event) {
+        var id = $(this).data("id");
+        var newEaten = $(this).data("newsleep");
+
+        var newdevoured = {
+            devoured: new newEaten
+        };
+
+        // Send the PUT request.
+        $.ajax("/api/burgers/" + id, {
+            type: "PUT",
+            data: newdevoured
+        }).then(
+            function () {
+                console.log("changed devoured to", newdevoured);
+                // Reload the page to get the updated list
+                location.reload();
+            }
+        );
+    });
+
+    $(".create-form").on("submit", function (event) {
+        // Make sure to preventDefault on a submit event.
+        event.preventDefault();
+
+        let newBurger = {
+            name: $("#burger").val(),
+        };
+
+        // Send the POST request.
+        $.ajax("/api/burgers", {
+            type: "POST",
+            data: newBurger
+        }).then(
+            function () {
+                console.log("created new burger");
+                // Reload the page to get the updated list
+                location.reload();
+            }
+        );
+    });
+});
